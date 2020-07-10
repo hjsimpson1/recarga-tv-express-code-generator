acaf0ea2c17932a191d1d842d50ce3377129218f
hjsimpson1 <68127324+hjsimpson1@users.noreply.github.com>
2020-07-10 16:58:12 -0300

    Update database-create.sql

--- database-create.sql
+++ database-create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE serial_codes (
     id INT PRIMARY KEY AUTO_INCREMENT,
     serial CHAR(16) UNIQUE NOT NULL,
-    user_email VARCHAR(255) DEFAULT NULL,
-    product ENUM('mensal', 'anual') DEFAULT NULL
+    user_email VARCHAR(255) querovideo1@gmail.com,
+    product ENUM('mensal', 'anual') anual
 );
